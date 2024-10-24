471cc19dae7ce9701e8569bce485c4322d705dfc
Hanaanee Hana <hanaanee.21@cse.mrt.ac.lk>
2024-10-24 22:33:51 +0530

    auto-commit

--- backend/mlapi.py
+++ backend/mlapi.py
@@ -6,12 +6,15 @@ from database import engine, get_db, SessionLocal
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 from typing import List
+from similarity import SimilarItemRec
 import pickle, keras
 import pandas as pd
 import numpy as np
 import trackmodel as trackmodel
 import track_playcount as track_playcount
 import json
+
+import cloudpickle
 
 
 
@@ -54,77 +57,13 @@ def uploadTrack():
     return {"message": "Data uploaded successfully"}
 
 
-
-
-# get similar tracks
-with open('model_for_similar_songs.pkl', 'rb') as file:
-    model = pickle.load(file)
-
-X_test_scaled = np.load('X_train_scaled.npy')
-music_info = pd.read_csv('Music Info.csv')
-df = music_info.drop(["name", "artist", "spotify_preview_url", "spotify_id", "tags"], axis=1)
-
-# @app.post("/getsimilartrack/{track_id}", status_code = status.HTTP_200_OK)
-# def get_similar_tracks_by_id(track_id : str, top_n: int = 10):
-#     filtered_df = df[df['track_id'] == track_id]
+## new model ---------------------------------------------
+with open('models\\new.pkl ', 'rb') as f:
+    new_model = cloudpickle.load(f)
     
-#     if filtered_df.empty:
-#         raise HTTPException(status_code=404, detail="Track ID not found")
-   
-#     track_index = filtered_df.index[0]
-#     track_features = X_test_scaled[track_index].reshape(1, -1)
-#     similarities = np.dot(X_test_scaled, track_features.T).flatten()
-#     similar_indices = np.argsort(similarities)[-top_n:]
-#     similar_track_ids = df.iloc[similar_indices]['track_id'].tolist()
-    
-#     similar_tracks_list = []
-#     for similar_track_id in similar_track_ids:
-#         track = db.query(trackmodel.track).filter(trackmodel.track.track_id == similar_track_id).first()
-#         if track:
-#             similar_tracks_list.append({
-#                 "track_id": track.track_id,
-#                 "track_name": track.name,
-#                 "artist": track.artist,
-#                 "genre": track.genre
-#             })
-    
-#     return {"similar_tracks": similar_tracks_list}
-
-
-import joblib
-
-knn_model = joblib.load('knn_model.pkl')
-
-df_encoded = pd.read_csv('df_encoded.csv')
-# with open('knn_model.pkl', 'rb') as model_file:
-#     knn_model = pickle.load(model_file)
-# Function to recommend similar tracks
 @app.post("/getsimilartrack/{track_id}", status_code = status.HTTP_200_OK)
 def recommend_similar_tracks(track_id:str, n_neighbors:int =10):
-    # Find the index of the given track_id
-    try:
-        track_index = df_encoded[df_encoded['track_id'] == track_id].index[0]
-    except IndexError:
-        print(f"Track ID {track_id} not found in the dataset.")
-        return []
-
-    # Get the row index of the specified track_id
-    track_index = df_encoded[df_encoded['track_id'] == track_id].index[0]
-
-    # Extract the feature vector using .iloc to get the row by index
-    feature_vector = df_encoded.drop(columns=['track_id']).iloc[track_index]
-
-    # Convert the Series to a DataFrame to retain feature names
-    feature_vector_df = feature_vector.to_frame().transpose()
-
-    # Find the nearest neighbors
-    distances, indices = knn_model.kneighbors(feature_vector_df)
-
-    # Exclude the input track itself from the results
-    similar_indices = indices[0][1:n_neighbors + 1]
-
-    # Get the track IDs of similar tracks
-    similar_tracks = df_encoded.iloc[similar_indices]['track_id'].tolist()
+    similar_tracks = new_model.get_recommendations(track_id,n_neighbors)[0]
     
     similar_tracks_list = []
     for similar_track_id in similar_tracks:
@@ -138,63 +77,86 @@ def recommend_similar_tracks(track_id:str, n_neighbors:int =10):
             })
     
     return {"similar_tracks": similar_tracks_list}
-
-
-
-
-# get popular tracks
-# @app.post("/playtrack/{track_id}")
-# def play_track(track_id: str, db: Session = Depends(get_db)):
-#     # Check if the track exists in the table
-#     track_record = db.query(track_playcount.trackcount).filter(track_playcount.trackcount.track_id == track_id).first()
     
-#     if track_record:
-#         # If the track exists, increment the playcount
-#         track_record.playcount += 1
-#     else:
-#         # If the track does not exist, add it with a playcount of 1
-#         new_track = track_playcount.trackcount(track_id=track_id, playcount=1)
-#         db.add(new_track)
     
-#     db.commit()
+       
+
+##old similar track model ---------------------------------------------
+
+
+# get similar tracks
+# with open('models\\model_for_similar_songs.pkl', 'rb') as file:
+#     model = pickle.load(file)
+
+# X_test_scaled = np.load('models\\X_train_scaled.npy')
+# music_info = pd.read_csv('models\\Music Info.csv')
+# df = music_info.drop(["name", "artist", "spotify_preview_url", "spotify_id", "tags"], axis=1)
+
+
+
+
+# import joblib
+
+# knn_model = joblib.load('models\\knn_model.pkl')
+
+# df_encoded = pd.read_csv('models\\df_encoded.csv')
+
+# @app.post("/getsimilartrack/{track_id}", status_code = status.HTTP_200_OK)
+# def recommend_similar_tracks(track_id:str, n_neighbors:int =10):
+#     # Find the index of the given track_id
+#     try:
+#         track_index = df_encoded[df_encoded['track_id'] == track_id].index[0]
+#     except IndexError:
+#         print(f"Track ID {track_id} not found in the dataset.")
+#         return []
+
+#     # Get the row index of the specified track_id
+#     track_index = df_encoded[df_encoded['track_id'] == track_id].index[0]
+
+#     # Extract the feature vector using .iloc to get the row by index
+#     feature_vector = df_encoded.drop(columns=['track_id']).iloc[track_index]
+
+#     # Convert the Series to a DataFrame to retain feature names
+#     feature_vector_df = feature_vector.to_frame().transpose()
+
+#     # Find the nearest neighbors
+#     distances, indices = knn_model.kneighbors(feature_vector_df)
+
+#     # Exclude the input track itself from the results
+#     similar_indices = indices[0][1:n_neighbors + 1]
+
+#     # Get the track IDs of similar tracks
+#     similar_tracks = df_encoded.iloc[similar_indices]['track_id'].tolist()
     
-#     return {"message": f"Track {track_id} playcount incremented."}
-
-
-
-
-# Assuming trackmodel and track_playcount are correctly defined and imported
-
-# @app.get("/popular_songs/")
-# def popular_songs(n: int = 10, db: Session = Depends(get_db)):
-#     results = (
-#         db.query(trackmodel.track, track_playcount.trackcount)
-#         .join(track_playcount.trackcount, trackmodel.track.track_id == track_playcount.trackcount.track_id)
-#         .order_by(track_playcount.trackcount.playcount.desc())
-#         .limit(n)
-#         .all()
-#     )
+#     similar_tracks_list = []
+#     for similar_track_id in similar_tracks:
+#         track = db.query(trackmodel.track).filter(trackmodel.track.track_id == similar_track_id).first()
+#         if track:
+#             similar_tracks_list.append({
+#                 "track_id": track.track_id,
+#                 "track_name": track.name,
+#                 "artist": track.artist,
+#                 "genre": track.genre
+#             })
     
-#     # Format results
-#     top_songs = [
-#         {
-#             "track_id": track.track_id,
-#             "track_name": track.name,
-#             "artist": track.artist,
-#             "genre": track.genre,
-#             "playcount": playcount.playcount
-#         }
-#         for track, playcount in results
-#     ]
-    
-#     return top_songs
+#     return {"similar_tracks": similar_tracks_list}
+
+## end-------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
 
 
 # Load your model and data
-with open('final_user_based.pkl', 'rb') as listening_file:
+with open('models\\final_user_based.pkl', 'rb') as listening_file:
     listening_model = pickle.load(listening_file)
     
-listening_df = pd.read_csv('User Listening History.csv')
+listening_df = pd.read_csv('models\\User Listening History.csv')
 aggregated_df = listening_df.groupby(['track_id', 'user_id'])['playcount'].sum().reset_index()
 all_tracks = set(aggregated_df['track_id'].unique())
 
